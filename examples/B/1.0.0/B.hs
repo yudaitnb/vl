1f056f2c99c5247814c03c869c9cc1f7b2e0dc3e
Yudai Tanabe <yudaitnb@DESKTOP-83BR481.localdomain>
2022-11-16 23:02:57 +0900

    tmp

--- examples/B/1.0.0/B.hs
+++ examples/B/1.0.0/B.hs
@@ -7,14 +7,3 @@ f x = x + 1
 g x = x + 1
 
 -- w x y = x + y
-
--- ll = length
-
--- map f xs = case xs of
---   [] -> []
---   hh : rst -> (f hh) : map f rst
-
--- length xs = case xs of
---   [] -> 0
---   hh : rst -> 1 + length rst
-
