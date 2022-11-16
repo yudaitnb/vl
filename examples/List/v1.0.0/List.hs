8e28b41d6ff92b94b8f69060c917c636e29f2e2c
Yudai Tanabe <yudaitnb@DESKTOP-83BR481.localdomain>
2022-11-16 23:02:57 +0900

    tmp

--- examples/List/v1.0.0/List.hs
+++ examples/List/v1.0.0/List.hs
@@ -1,14 +1,14 @@
 module List where
 
 head xs = case xs of
-  []     -> 0
-  hh:rst -> hh
+  []    -> 0
+  h:rst -> h
 
 length xs = case xs of
   []     -> 0
   hh:rst -> 1 + length rst
 
 last xs = case xs of
-  []     -> 0
-  hh:[]  -> hh
-  hh:rst -> last rst
+  []    -> 0
+  h:[]  -> h
+  h:rst -> last rst
